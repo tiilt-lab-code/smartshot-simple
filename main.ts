8b67e27ecea97e107ced6aeb889f0ba4323b2d39
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2023-02-08 20:41:54 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-loops.everyInterval(500, function () {
-	
-})
 basic.forever(function () {
     if (input.rotation(Rotation.Pitch) >= 90 && input.rotation(Rotation.Pitch) <= 135) {
         basic.showIcon(IconNames.Target)
